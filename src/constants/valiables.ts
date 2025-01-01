6f5a9c13fa5746a081ea842218de93352ba52128
BORDINSAK-P\bordinsak-p <006469@cdg.co.th>
2025-01-01 22:44:49 +0700

    feat(user service): add/update user service

--- src/constants/valiables.ts
+++ src/constants/valiables.ts
@@ -1,4 +1,13 @@
 export default class ValiableConstants {
-    public static readonly API_URL = '/api/v1';
+  public static readonly API_URL = "/api/v1";
+  public static readonly STATUS_CODE = {
+    OK: 200,
+    CREATED: 201,
+    NO_CONTENT: 204,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    INTERNAL_SERVER_ERROR: 500,
+  };
 }
-
